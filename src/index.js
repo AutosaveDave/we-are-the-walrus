1d4519f054235b3ac95560d1c56fd9888e0b9fbf
AutosaveDave <106124456+dccoppock@users.noreply.github.com>
2024-01-27 21:59:56 -0500

    maybe now?

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import App from './App';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
-  {
-    path: "game/:game_id",
-    element: <App currentView={'game'}/>,
-    loader: ( { params } ) => params.game_id,
-  },
-  {
-    path: "/",
-    element: <App currentView={'site'}/>,
-    loader: ( { params } ) => false,
-  },
-], { basename: process.env.PUBLIC_URL } );
-console.log(process.env.PUBLIC_URL)
+      {
+        path: "game/:game_id",
+        element: <App currentView={'game'}/>,
+        loader: ( { params } ) => params.game_id,
+      },
+      {
+        path: "/",
+        element: <App currentView={'site'}/>,
+        loader: ( { params } ) => false,
+      }, 
+], { basename: process.env.REACT_APP_PUBLIC_URL } );
+console.log(process.env.REACT_APP_PUBLIC_URL)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
